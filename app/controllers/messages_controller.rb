2f1578a608f75b8d269fbb85118e54f447fb078b
taka0707 <yukken777@gmail.com>
2020-07-08 19:14:38 +0900

    edit-message_system
    add-message_model
    add-time_formats.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.includes(:user)
+    # binding.pry
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  
+  def message_params
+    params.permit(:content).merge(user_id: current_user.id)
+  end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :messages
+
   validates :nickname, presence: true, uniqueness: true
 end
